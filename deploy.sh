a6acba7c5a6dd4d607b2ae5b7a717e8cdd8c55de
Alif Faishol <alifaishol@gmail.com>
2020-01-28 14:53:56 +0700

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-
-git checkout master
-git pull https://${CI_DEPLOY_USER}:${CI_DEPLOY_PASSWORD}@gitlab.com/alifaishol/interoperability-2017-deployment.git master
-git submodule update
 docker-compose exec app bash -c 'cd alif && composer install && php artisan:migrate'
 
 exit 0
