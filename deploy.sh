1e1742345d1e9d30c974feaed3109ffc459ef266
Alif Faishol <alifaishol@gmail.com>
2020-01-28 18:15:46 +0700

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker-compose up -d
-docker-compose exec app bash -c "cd alif && composer install && php artisan:migrate && exit"
-docker-compose exec app bash -c "cd eep && composer install && php artisan:migrate && exit"
+docker-compose exec -T app bash -c "cd alif && composer install && php artisan migrate && exit"
+docker-compose exec -T app bash -c "cd eep && composer install && php artisan migrate && exit"
 
 exit 0
