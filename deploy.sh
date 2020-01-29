c3e7e74c1ba7f89dd8da656b9d58f4e8ac5e2703
Alif Faishol <alifaishol@gmail.com>
2020-01-29 21:44:17 +0700

    add muzib

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@
 docker-compose up -d
 docker-compose exec -T app bash -c "cd alif && composer install && php artisan migrate && exit"
 docker-compose exec -T app bash -c "cd eep && composer install && php artisan migrate && exit"
+docker-compose exec -T app bash -c "cd muzib && composer install && php artisan migrate && exit"
 
 exit 0
