71cc386b3c280ee5d9bfd7e64abd7a4ee9078026
Alif Faishol <alifaishol@gmail.com>
2020-01-31 09:40:25 +0700

    add fajar

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ docker-compose up -d
 cp .env.base ./tedc-sw-interoperability-exam/src/.env
 echo "DB_DATABASE=space_reservation" >> ./tedc-sw-interoperability-exam/src/.env
 echo "APP_URL=http://alif.2017a.site" >> ./tedc-sw-interoperability-exam/src/.env
-
 docker-compose exec -T app bash -c "cd alif && composer install && php artisan migrate && exit"
 
 cp .env.base ./UAS-PengelolaTurnamen/.env
@@ -17,5 +16,10 @@ cp .env.base ./E-Voting-KPU/.env
 echo "DB_DATABASE=e_voting" >> ./E-Voting-KPU/.env
 echo "APP_URL=http://muzib.2017a.site" >> ./E-Voting-KPU/.env
 docker-compose exec -T app bash -c "cd muzib && composer install && php artisan migrate && exit"
+
+cp .env.base ./UAS_Booking/.env
+echo "DB_DATABASE=booking_restoran" >> ./UAS_Booking/.env
+echo "APP_URL=http://fajar.2017a.site" >> ./UAS_Booking/.env
+docker-compose exec -T app bash -c "cd fajar && composer install && php artisan migrate && exit"
 
 exit 0
